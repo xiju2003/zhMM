d1d356829f1fec0f6b7aa3b6de0b926cfd6f6150
SUNNNNNNN <102894611+sh3llCat2007@users.noreply.github.com>
2022-06-10 06:24:03 -0700

    likeeeeee

---
+++ lib/data.py
@@ -0,0 +1,14 @@
+import  sys
+#import  plantform
+import  os
+#OS_INFO =   plantform.plantform()
+#CPU_TYPE=   plantform.machine()
+
+OPSPEN_PATH =   os.path.join(os.path.dirname(os.path.realpath(__file__)), "../")
+POCS_PATH   =   os.path.join(OPSPEN_PATH, "PoCs")
+EXPS_PATH   =   os.path.join(OPSPEN_PATH, "exploits")
+REPORT_PATH =   os.path.join(OPSPEN_PATH, "reports")
+
+VERSION =   "v01 dev"
+
+POCS    =   []
